5421b938f2bbd997e3cc2eefab67a21b447acc6d
João Bianchim <joaobianchim@gmail.com>
2022-03-25 15:48:19 -0300

    Separação Cadastro Produtor/Cliente

--- AgroShop/website/auth.py
+++ AgroShop/website/auth.py
@@ -11,11 +11,33 @@ def login():
 def logout():
     return("<p>Logout</p>")
 
-@auth.route('/cadastrar', methods = ['GET', 'POST'])
-def sign_up():
+@auth.route('/cadastrar/cliente', methods = ['GET', 'POST'])
+def sign_up_client():
     if request.method == 'POST':
         email = request.form.get('email')
         nome = request.form.get('nome')
+        senha1 = request.form.get('senha1')
+        senha2 = request.form.get('senha2')
+
+        if len(email) < 4:
+            flash('O email deve conter pelo menos caracteres.', category='error')
+        elif len(nome) < 2:
+            flash('O nome deve conter pelo menos 3 caracteres.', category='error')
+        elif senha1 != senha2:
+            flash('Senhas não são iguais.', category='error')
+        elif len(senha1) < 7:
+            flash('A senha deve ter pelo menos 8 caracteres.', category='error')
+        else:
+            flash('Account created!', category='success')
+    return render_template("sign-up-client.html")
+
+@auth.route('/cadastrar/produtor', methods = ['GET', 'POST'])
+def sign_up_prod():
+    
+    if request.method == 'POST':
+        email = request.form.get('email')
+        nome = request.form.get('nome')
+        cpf = request.form.get('cpf')
         senha1 = request.form.get('senha1')
         senha2 = request.form.get('senha2')
 
@@ -29,4 +51,5 @@ def sign_up():
             flash('A senha deve ter pelo menos 8 caracteres.', category='error')
         else:
             flash('Account created!', category='success')
-    return render_template("sign-up.html")
+    
+    return render_template("sign-up-prod.html")
--- AgroShop/website/views.py
+++ AgroShop/website/views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template("home.html")
+
+@views.route('/cadastrar', methods = ['GET', 'POST'])
+def tipocadastro():      
+    return render_template("tipo-cadastro.html")
