b0de0ad03a5429921c13348d34181a3822472314
Jihad Rahman <jihad.rahman@mottu.com.br>
2022-04-04 22:44:30 -0300

    Criar um banco de dados com as tabelas cliente e produtor.

--- AgroShop/website/__init__.py
+++ AgroShop/website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import  SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'Grupo _4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -10,4 +17,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import Cliente, Produtor 
+
+    criar_dataBase(app)
+
     return app
+
+def criar_dataBase(app):
+    if not path('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('DataBase Criado!')
---
+++ AgroShop/website/models.py
@@ -0,0 +1,19 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+#Tabelas
+
+class Cliente(db.Model, UserMixin):
+    #Colunas
+    id = db.Column.make_declarative_base.Integer, primary_key=True
+    email = db.Column(db.String(150), unique = True)#maximo 150 caracteres, email repetido nao vale(unique)
+    senha = db.Column(db.String(150))
+    nome = db.Column(db.String(150))
+   
+class Produtor(db.Model, UserMixin):
+    
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique = True)
+    senha = db.Column(db.String(150))
+    nome = db.Column(db.String(150))
