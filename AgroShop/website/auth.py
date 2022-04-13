57dfd42e83c62a553513548e86eb3e7d50e505ed
João Bianchim <joaobianchim@gmail.com>
2022-04-13 00:53:07 -0300

    Finalização do Banco para Cadastro e Login (Cliente),
    Co-authored-by: João Bianchim <joao.bianchim@aluno.ufabc.edu.br>
    Co-authored-by: Jihad-Surf <jihad.a@aluno.ufabc.edu.br>

--- AgroShop/website/auth.py
+++ AgroShop/website/auth.py
@@ -1,10 +1,25 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from website import views
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import Cliente
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/entrar', methods = ['GET', 'POST'])
 def login():
-    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        senha = request.form.get('senha')
+        usuario = Cliente.query.filter_by(email=email).first()
+        if usuario:
+            if check_password_hash(usuario.senha, senha):
+                flash('Login efetuado com Sucesso!', category='success')
+            else:
+                flash('Senha Incorreta', category='error')
+        else:
+            flash('E-mail não cadastrado', category='error') 
+        
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -19,7 +34,10 @@ def sign_up_client():
         senha1 = request.form.get('senha1')
         senha2 = request.form.get('senha2')
 
-        if len(email) < 4:
+        usuario = Cliente.query.filter_by(email=email).first()
+        if usuario:
+            flash('Email já cadastrado', category='error')
+        elif len(email) < 4:
             flash('O email deve conter pelo menos caracteres.', category='error')
         elif len(nome) < 2:
             flash('O nome deve conter pelo menos 3 caracteres.', category='error')
@@ -28,7 +46,12 @@ def sign_up_client():
         elif len(senha1) < 7:
             flash('A senha deve ter pelo menos 8 caracteres.', category='error')
         else:
+            new_user = Cliente(email=email, senha=generate_password_hash(senha1, method='sha256'), nome=nome)
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
     return render_template("sign-up-client.html")
 
 @auth.route('/cadastrar/produtor', methods = ['GET', 'POST'])
